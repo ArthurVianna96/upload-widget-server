eca3858a90d5d2c6176ebe24a8db1e8636bf19ac
Arthur <arthurpedrosa96.ap@gmail.com>
2025-01-18 14:43:22 -0300

    added containers, tests and documentation

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,56 @@
+import { uploadImageRoute } from '@/routes/upload-image'
 import { fastifyCors } from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
+import {
+	hasZodFastifySchemaValidationErrors,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, _request, reply) => {
+	if (hasZodFastifySchemaValidationErrors(error)) {
+		return reply.status(400).send({
+			message: 'Validation error',
+			issues: error.validation,
+		})
+	}
+
+	// Envia o erro p/ alguma ferramenta de observabilidade (Sentry/Datadog/Grafana/OTel)
+
+	console.error(error)
+
+	return reply.status(500).send({
+		message: 'Internal server error.',
+	})
+})
+
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: 'Upload Server',
+			version: '1.0.0',
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+	routePrefix: '/docs',
+})
+
+server.register(uploadImageRoute)
 
 server
 	.listen({ port: 3333, host: '0.0.0.0' })
