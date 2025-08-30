f388ec0612296c6500dadfb66375db6b15fb3509
Arthur <arthurpedrosa96.ap@gmail.com>
2025-08-30 18:59:33 -0300

    change health check message

--- src/infra/http/routes/health-check.ts
+++ src/infra/http/routes/health-check.ts
@@ -16,7 +16,7 @@ export const healthCheckRoute: FastifyPluginAsyncZod = async server => {
 			},
 		},
 		async (request, reply) => {
-			return reply.status(200).send({ message: 'App running...' })
+			return reply.status(200).send({ message: 'OK!' })
 		}
 	)
 }
